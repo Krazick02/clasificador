ae446afc43a2d1d25d29fdadb121e8b39ac22266
Krazick02 <63378450+Krazick02@users.noreply.github.com>
2022-11-30 22:39:53 -0700

    Otro avance 2

--- redes.py
+++ redes.py
@@ -1,45 +1,43 @@
 import pandas as pd
 from pandas.plotting import scatter_matrix
 from sklearn.model_selection import train_test_split
-#from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import GaussianNB
 import matplotlib.pyplot as plt
-from sklearn.metrics import classification_report
 import seaborn as sns
 from sklearn.inspection import DecisionBoundaryDisplay
-#from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
 from matplotlib.colors import ListedColormap
-from sklearn.neural_network import MLPClassifier 
+
 
 dataset = pd.read_csv("datosPesoCi3.csv",)
 
 #Caracteristicas
 X = dataset[['peso','cirf']].values
-
 #Clases
 y = dataset['clasificacion'].values
 print(dataset.shape)
 print(dataset.head(20))
 print(dataset.describe())
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.20, random_state=73)
-clf = MLPClassifier(hidden_layer_sizes=(11,20,1),solver='lbfgs',alpha=1e-5,random_state=1,max_iter=100)
-clf.fit(X_train,y_train)
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.70, random_state=73)
+
+gnb = GaussianNB()
+y_pred = gnb.fit(X_train, y_train).predict(X_test)
+
+print("El numero de puntos mal etiquetados de un totalde %d points  es: %d"
+      % (X_test.shape[0], (y_test != y_pred).sum()))
 
 #Clasificamos un nuevo dato
 DatoNuevo=[[171	,22.5]]
-prediccion = clf.predict(DatoNuevo)
-#predictions = lda.predict(X_test)
+prediccion = gnb.predict(DatoNuevo)
 print("El nuevo dato pertenece a", prediccion)
-print("El numero de puntos mal etiquetados de un total de %d puntos es: %d" % (X_test.shape[0], (y_test != prediccion).sum()))
 
 ### Create color maps
 cmap_light = ListedColormap(["#FFAAAA", "#AAFFAA"])
 cmap_bold = ["#FF0000",  "#00FF00"]
-
-
 _, ax = plt.subplots()
 DecisionBoundaryDisplay.from_estimator(
-    clf,
+    gnb,
     X,
     cmap=cmap_light,
     ax=ax,
@@ -50,7 +48,7 @@ DecisionBoundaryDisplay.from_estimator(
     shading="auto",
 )
 
-# Plot also the training points
+# Dibujar el dato nuevo
 sns.scatterplot(
     x=X[:, 0],
     y=X[:, 1],
@@ -67,6 +65,6 @@ sns.scatterplot(
     edgecolor="yellow",)
     
     
-plt.title("Maquina de vector soporte" )
+plt.title("Clasificador de Bayes" )
 
 plt.show()
